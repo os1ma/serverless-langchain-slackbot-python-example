a9a3ed630b6fb1993e681e1a8c3c60d3ce0e6f2b
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-07-08 08:01:04 +0900

    Add local_handler

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import time
 
 from slack_bolt import App
 from slack_bolt.adapter.aws_lambda import SlackRequestHandler
@@ -11,13 +12,14 @@ logger = logging.getLogger(__name__)
 app = App(
     signing_secret=os.environ["SLACK_SIGNING_SECRET"],
     token=os.environ["SLACK_BOT_TOKEN"],
-    process_before_response=True
+    process_before_response=True,
 )
+
 
 def just_ack(ack):
     ack()
 
-import time
+
 def run_long_process(say, body):
     logger.info("sleeping...")
     say("sleeping...")
@@ -25,12 +27,24 @@ def run_long_process(say, body):
     logger.info("sleep finished")
     say("sleep finished")
 
-app.event("app_mention")(
-    ack=just_ack,
-    lazy=[run_long_process]
-)
+
+app.event("app_mention")(ack=just_ack, lazy=[run_long_process])
+
 
 def handler(event, context):
+    """Lambda関数のエントリーポイント"""
     logger.info("handler called")
     slack_handler = SlackRequestHandler(app=app)
     return slack_handler.handle(event, context)
+
+
+def local_main():
+    """ローカルで実行するためのエントリーポイント"""
+    from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+    local_handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
+    local_handler.start()
+
+
+if __name__ == "__main__":
+    local_main()
