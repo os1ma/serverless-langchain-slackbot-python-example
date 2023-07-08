58eba01150b17e652715a662c23ef9f7a96df666
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-07-08 09:38:23 +0900

    Use LangChain model

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import logging
 import os
 import time
+from typing import Any
 
-import openai
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import LLMResult
 from slack_bolt import App
 from slack_bolt.adapter.aws_lambda import SlackRequestHandler
 
@@ -20,6 +23,30 @@ app = App(
     process_before_response=True,
 )
 
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
+
+
+class SlackStreamingCallbackHandler(BaseCallbackHandler):
+    last_send_time = time.time()
+    message = ""
+
+    def __init__(self, channel, ts):
+        self.channel = channel
+        self.ts = ts
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.message += token
+
+        now = time.time()
+        if now - self.last_send_time > CHAT_UPDATE_INTERVAL_SEC:
+            self.last_send_time = now
+            app.client.chat_update(
+                channel=self.channel, ts=self.ts, text=f"{self.message}..."
+            )
+
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
+        app.client.chat_update(channel=self.channel, ts=self.ts, text=self.message)
+
 
 def chat(say, body):
     logger.info("chat start")
@@ -31,26 +58,9 @@ def chat(say, body):
     say_response = say("Thinking...")
     ts = say_response["ts"]
 
-    messages = [
-        {"role": "user", "content": prompt},
-    ]
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=messages, temperature=0, stream=True
-    )
+    callback = SlackStreamingCallbackHandler(channel=channel, ts=ts)
+    llm.predict(prompt, callbacks=[callback])
 
-    message = ""
-    last_send_time = time.time()
-
-    for chunk in response:
-        delta = chunk["choices"][0]["delta"].get("content", "")
-        message += delta
-
-        now = time.time()
-        if now - last_send_time > CHAT_UPDATE_INTERVAL_SEC:
-            last_send_time = now
-            app.client.chat_update(channel=channel, ts=ts, text=f"{message}...")
-
-    app.client.chat_update(channel=channel, ts=ts, text=message)
     logger.info("chat end")
 
 
